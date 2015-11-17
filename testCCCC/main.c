8347901d69022063578debcfb5ce3664467ba48f
Capotasto <rocker519804@gmail.com>
2015-11-16 16:20:28 -0800

    Leaning else if sentence and Ascii code

--- testCCCC/main.c
+++ testCCCC/main.c
@@ -10,47 +10,38 @@
 
 int main(int argc, const char * argv[]) {
     
-    int x, y; //defined an integer variable
-    //int x2;
-    //int y,z,m,d, a;
-    //float x3; //defined a floating point number variable
-    //char x4; //defined a character variable
-
-    //Show some number example
-    //printf("Please enter another integer number: ");
-    //scanf("%i", &x2);
-    //printf("you entered this value: %i and %p.\n\n\n", x1, &x2);
-    //printf("you entered this value: %i and %i.\n\n\n", x1, x2);
+    char alphabet;
+    printf("Please enter a character between a to z: ");
+    scanf("%c", &alphabet);
     
-    // Calculations number example
-//    printf("Please enter an integer number: ");
-//    scanf("%i,%i", &x1,&x2);
-//    y = x1 + x2;
-//    z = x1 * x2;
-//    m = x1 - x2;
-//    d = x1 / x2;
-//    a = x1 % x2;
-//    printf("you entered this value: %i and %i .\n\n\n", y,z);
-//    printf("you entered this value: %i and %i and %i.\n\n\n", m,d,a);
-    
-    //Conditional example
-//    printf("Please enter another integer number: ");
-//    scanf("%i", &x);
-//    if (x1 > 0) {
-//        printf("The number is positive.\n");
-//    }else if( x1 == 0){
-//        printf("The number is ZERO.\n");
-//    }else{
-//        printf("The number is negative.\n");
-//    }
-    printf("Please enter another integer number: ");
-    scanf("%i", &x);
-    
-    y = x % 2;
-    if (y == 0) {
-        printf("The number is even (devisible by 2)\n");
+    if ('a' <= alphabet && alphabet <= 'z') {
+        if (alphabet  == 'a') {
+            printf("A\n");
+        }else if(alphabet  == 'b'){
+            printf("B\n");
+        }else if(alphabet  == 'c'){
+            printf("C\n");
+        }else if(alphabet  == 'd'){
+            printf("D\n");
+        }else if(alphabet  == 'e'){
+            printf("E\n");
+        }else if(alphabet  == 'f'){
+            printf("F\n");
+        }else if(alphabet  == 'g'){
+            printf("G\n");
+        }else if(alphabet  == 'h'){
+            printf("H\n");
+        }else if(alphabet  == 'i'){
+            printf("I\n");
+        }else if(alphabet  == 'j'){
+            printf("J\n");
+        }else if(alphabet  == 'k'){
+            printf("K\n");
+        }else if(alphabet  == 'l'){
+            printf("L\n");
+        }
     }else{
-        printf("The number is odd (not devisible by 2)\n");
+        printf("It's not a to z word.\n");
     }
     
     return 0;
