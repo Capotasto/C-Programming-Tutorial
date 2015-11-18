bbb6b240785280efb897b9ae31260468dddac5a4
Capotasto <rocker519804@gmail.com>
2015-11-18 12:47:18 -0800

    conditional in the for sentence

--- testCCCC/main.c
+++ testCCCC/main.c
@@ -10,24 +10,21 @@
 
 int main(int argc, const char * argv[]) {
     
-    char letter;
-    int x;
-    printf("Please enter a letter: ");
-    scanf("%c", &letter);
-    
-    //if ('a' <= letter && letter <= 'z') {
-    if (97 <= letter && letter <= 122) {
-        x = letter -32;
-        printf("the UpperCase is: %c\n",x);
-        
-    }else if('A' <= letter && letter <= 'Z') {
-        x = letter + 32;
-        printf("the LowerCase is: %c\n",x);
-    
-    }else{
-        x = letter;
-        printf("You type charactor is %c\n Type uppercase alphabet.",x);
+    int num,rep;
+    int sum = 0;
+
+    for (rep = 0; rep <= 0 || 20 <= rep;) {
+        printf("How many times do you want to repeat?: ");
+        scanf("%i",&rep);
     }
+    
+    for (int i= 0; i < rep; i++) {
+        printf("Enter a number: ");
+        scanf("%i",&num);
+        sum = sum + num;
+    }
+    
+    printf("Sum: %i\n",sum);
     
     return 0;
 }
