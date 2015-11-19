83376fb2178cda2aa7ed09e357c374a2ae29fc76
Capotasto <rocker519804@gmail.com>
2015-11-18 16:22:59 -0800

    Use Switch sentence

--- testCCCC/main.c
+++ testCCCC/main.c
@@ -11,27 +11,54 @@
 int main(int argc, const char * argv[]) {
     
     int x;
-
-//    for (; i >=0; ) {
-//        scanf("%i",&x);
-//        printf("you entered: %i",x);
-//
-//    }
     
-//    while (x >= 0) {
-//        printf("Enter a number: \n");
-//        scanf("%i",&x);
-//        printf("you entered: %i\n",x);
-//    }
-    
-    
-    do {
+    while (x >= 0) {
         printf("Enter a number: \n");
         scanf("%i",&x);
-        printf("you entered: %i\n",x);
-    } while (x >= 0);
-    
-    
+
+        switch (x) {
+            case 1:
+                printf("January\n");
+                break;
+            case 2:
+                printf("February\n");
+                break;
+            case 3:
+                printf("March\n");
+                break;
+            case 4:
+                printf("April\n");
+                break;
+            case 5:
+                printf("May\n");
+                break;
+            case 6:
+                printf("June\n");
+                break;
+            case 7:
+                printf("July\n");
+                break;
+            case 8:
+                printf("August\n");
+                break;
+            case 9:
+                printf("September\n");
+                break;
+            case 10:
+                printf("October\n");
+                break;
+            case 11:
+                printf("November\n");
+                break;
+            case 12:
+                printf("December\n");
+                break;
+                
+            default:
+                printf("Enter a number between 1 to 12\n");
+                break;
+        }
+    }
     
     return 0;
 }
