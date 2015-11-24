598c8bfbcd429fefb8f984d2e2cad21785c693b0
Capotasto <rocker519804@gmail.com>
2015-11-23 17:41:16 -0800

    Printing the number of each digit

--- testCCCC/main.c
+++ testCCCC/main.c
@@ -7,31 +7,74 @@
 //
 
 #include <stdio.h>
+#include <math.h>
 
 int main(int argc, const char * argv[]) {
     
-    int g[10];
-    int sum = 0;
-    int i;
+    int inputNum;
+    int calcNum;
+    int inputDigitNum = 0;
+    int digitNum = 0;
+    int remeinNum = 0;
     
-    g[0] = 20;
-    g[1] = 13;
-    g[2] = 10;
-    g[3] = 11;
-    g[4] = 45;
-    g[5] = 13;
-    g[6] = 10;
-    g[7] = 11;
-    g[8] = 45;
-    g[9] = 35;
+    printf("Enter a Number: ");
+    scanf("%i", &inputNum);
     
-    for (i = 0; i < 10; i++) {
-        printf("g[%i] is equal to %i\n",i,g[i]);
-        sum += g[i]; //sum = sum + g[i]
+    //Calcurate Digit number of input number
+    calcNum = inputNum;
+    while (calcNum != 0) {
+        calcNum = calcNum / 10;
+        inputDigitNum++;
+    }
+    
+    for (int i = 0; i < inputDigitNum; i++) {
+        if (i ==0) {
+            printf("digit is %i\n", inputNum/(int)pow(10,inputDigitNum-i-1));
+            remeinNum = inputNum % (int)pow(10,inputDigitNum-i-1);
+            calcNum = remeinNum;
+        }else{
+            //Calcurate Digit number of input number
+            
+            digitNum = 0;
+            while (calcNum != 0) {
+                calcNum = calcNum / 10;
+                digitNum++;
+            }
+            if (digitNum != inputDigitNum-i) {
+                printf("digit is 0\n");
+            }else{
+                printf("digit is %i\n", remeinNum/(int)pow(10,inputDigitNum-i-1));
+                remeinNum = remeinNum % (int)pow(10,inputDigitNum-i-1);
+            }
+            calcNum = remeinNum;
+            
+        }
         
     }
-    printf("Sum is %i\n",sum);
-    printf("Average num %f\n",(float)sum/(float)i);
+    
+    
+    //    char inputChar;
+    //
+    //    printf("Enter a alphabet:");
+    //    scanf("%c",&inputChar);
+    //
+    //    switch (inputChar) {
+    //        case 'a':
+    //        case 'A':
+    //        case 'e':
+    //        case 'E':
+    //        case 'i':
+    //        case 'I':
+    //        case 'o':
+    //        case 'O':
+    //        case 'u':
+    //        case 'U':
+    //            printf("You entered a vowel.\n");
+    //            break;
+    //        default:
+    //            printf("You entered other types of alphabet is not vowel {a,e,i,o,u}\n");
+    //            break;
+    //    }
     
     
     return 0;
