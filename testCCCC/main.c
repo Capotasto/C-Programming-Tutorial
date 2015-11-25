403f1952eba79f01d1b8416bd874efdf2ee0a5f1
Capotasto <rocker519804@gmail.com>
2015-11-24 16:56:30 -0800

    Getting Prime Number

--- testCCCC/main.c
+++ testCCCC/main.c
@@ -11,41 +11,63 @@
 
 int main(int argc, const char * argv[]) {
     
+    int limitCount;
+    int isPrimeNumber;
     
-    int compareCount;
+    printf("What number is limit to get prime number?: ");
+    scanf("%i",&limitCount);
     
-    printf("How many number do you have and compare?: ");
-    scanf("%i",&compareCount);
-    
-    int inputNum[compareCount];
-    
-    for (int i = 0; i < compareCount; i++) {
-        printf("Please Enter %i number One by one: ",compareCount);
-        scanf("%i",&inputNum[i]);
-    }
-    
-    while (1) {
-        //SortCounter
-        int sortCounter = 0;
+    for (int i = 2; i <= limitCount; i++) {
         
-        for (int i = 0; i < compareCount-1; i++) {
-            if (inputNum[i+1] < inputNum[i])  {
-                //Swapping
-                int t;
-                t = inputNum[i+1];
-                inputNum[i+1] = inputNum[i];
-                inputNum[i] = t;
-                sortCounter++;
+        isPrimeNumber = 1;
+        
+        for (int j = 1; j <= i; j++) {
+            if (j != 1 && j != i) {
+                if (i % j == 0) {
+                    isPrimeNumber = 0;
+                }
             }
         }
-        if (sortCounter == 0) {
-            break;
+        if (isPrimeNumber == 1) {
+            printf("%i is Prime number!\n", i);
         }
     }
     
-    for (int k = 0; k < compareCount ; k++) {
-        printf("Sorted Number: %i\n",inputNum[k]);
-    }
+    
+//    int compareCount;
+//    
+//    printf("How many number do you have and compare?: ");
+//    scanf("%i",&compareCount);
+//    
+//    int inputNum[compareCount];
+//    
+//    for (int i = 0; i < compareCount; i++) {
+//        printf("Please Enter %i number One by one: ",compareCount);
+//        scanf("%i",&inputNum[i]);
+//    }
+//    
+//    while (1) {
+//        //SortCounter
+//        int sortCounter = 0;
+//        
+//        for (int i = 0; i < compareCount-1; i++) {
+//            if (inputNum[i+1] < inputNum[i])  {
+//                //Swapping
+//                int t;
+//                t = inputNum[i+1];
+//                inputNum[i+1] = inputNum[i];
+//                inputNum[i] = t;
+//                sortCounter++;
+//            }
+//        }
+//        if (sortCounter == 0) {
+//            break;
+//        }
+//    }
+//    
+//    for (int k = 0; k < compareCount ; k++) {
+//        printf("Sorted Number: %i\n",inputNum[k]);
+//    }
     
     return 0;
 }
