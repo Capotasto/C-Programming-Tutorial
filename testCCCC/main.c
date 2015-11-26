aebfb7144a0cbbe0e233e131bae03b7daf884740
Capotasto <rocker519804@gmail.com>
2015-11-26 13:57:23 -0800

    Decimal to Hex and oppsit way

--- testCCCC/main.c
+++ testCCCC/main.c
@@ -12,77 +12,128 @@
 
 int main(int argc, const char * argv[]) {
     
-    char input[20];
     int inputSize;
     int i;
     int decimalNum;
+    int baseNumber;
+    int digitNumber;
+    char binaryNum[100];
+    char hexNum[100];
     
-    printf("Please Enter Binary Number: ");
-    gets(input);
+    printf("Please Enter Base Number: ");
+    scanf("%i",&baseNumber);
+    
+    printf("Please Enter Number of digit: ");
+    scanf("%i",&digitNumber);
+    
+    char input[digitNumber];
+    
+    printf("Please Enter some Number of the base you decided: ");
+    scanf("%s",input);
     
     inputSize = (int)strlen(input);
     
     decimalNum = 0;
-    for (i = 0; i < inputSize; i++) {
-        decimalNum += (input[i]-'0') * pow((float)2, inputSize-i-1);
-        printf("fk %i\n",decimalNum);
-        
+    switch (baseNumber) {
+        case 2:
+            for (i = 0; i < inputSize; i++) {
+                decimalNum += (input[i]-'0') * pow((float)baseNumber, inputSize-i-1);
+                printf("fk %i\n",decimalNum);
+                
+            }
+            break;
+            
+        case 16:
+            for (i = 0; i < inputSize; i++) {
+                switch (input[i]) {
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        decimalNum += (input[i]-'0') * pow((float)baseNumber, inputSize-i-1);
+                        printf("fk %i\n",decimalNum);
+                        break;
+                        
+                    case 'A':
+                    case 'B':
+                    case 'C':
+                    case 'D':
+                    case 'E':
+                    case 'F':
+                    case 'a':
+                    case 'b':
+                    case 'c':
+                    case 'd':
+                    case 'e':
+                    case 'f':
+                        decimalNum += (input[i]-'7') * pow((float)baseNumber, inputSize-i-1);
+                        printf("fk %i\n",decimalNum);
+                        break;
+                    default:
+                        break;
+                }
+                
+            }
+            
+            break;
+        default:
+            break;
     }
     
     printf("Decimal Number is %i\n",decimalNum);
-    
-    
-    
-//    int limitNumber;
-//    int divideFlg = 1;
-//    int quotientNum;
-//    int remainder;
-//    
-//    printf("What Decimal number is to get get Binary numer : ");
-//    scanf("%i",&limitNumber);
-//    
-//    int i = 0;
-//    int tempNum = limitNumber;
-//    while (divideFlg) {
-//        quotientNum = tempNum/2;
-//        if (quotientNum == 1) {
-//            divideFlg = 0;
-//        }else{
-//            tempNum = quotientNum;
-//        }
-//        i++;
-//    }
-//    
-//    int binaryNum[i+1];
-//    
-//    
-//    divideFlg = 1;
-//    i =0;
-//    while (divideFlg) {
-//        
-//        quotientNum = limitNumber/2;
-//        remainder = limitNumber%2;
-//        
-//        if (quotientNum == 1) {
-//            divideFlg = 0;
-//            binaryNum[i] = remainder;
-//            binaryNum[i+1] = quotientNum;
-//
-//        }else{
-//            limitNumber = quotientNum;
-//            binaryNum[i] = remainder;
-//
-//        }
-//        i++;
-//    }
-//    printf("The binay number is ");
-//    for (int j= 0; j < i+1; j++) {
-//        printf("%i",binaryNum[i-j]);
-//
-//    }
-//    printf("\n");
 
     
+    //**  Decimal to some base of number user decide **//
+    printf("Plsease Enter the base number to convert Dec to what?:");
+    scanf("%i", &baseNumber);
+    
+    inputSize = (int)strlen(input);
+    int divideFlg;
+    int quotientNum;
+    int remainder;
+    
+    switch (baseNumber) {
+        case 2:
+            divideFlg = 1;
+            i =0;
+            while (divideFlg) {
+                
+                quotientNum = decimalNum / 2;
+                remainder = decimalNum % 2;
+                
+                if (quotientNum == 1) {
+                    divideFlg = 0;
+                    binaryNum[i] = remainder;
+                    binaryNum[i+1] = quotientNum;
+                    
+                }else{
+                    decimalNum = quotientNum;
+                    binaryNum[i] = remainder;
+                    
+                }
+                i++;
+            }
+            
+            printf("The binay number is ");
+            for (int j= 0; j < i+1; j++) {
+                printf("%i",binaryNum[i-j]);
+                
+            }
+            
+            break;
+        
+        case 16:
+            
+            break;
+        default:
+            break;
+    }
     
     
     return 0;
