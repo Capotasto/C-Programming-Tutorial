f44a3564b5921508c615c2dda5a3416a5c90aae9
Capotasto <rocker519804@gmail.com>
2015-11-25 17:43:24 -0800

    Get the decimal number form binary number

--- testCCCC/main.c
+++ testCCCC/main.c
@@ -8,57 +8,79 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
 int main(int argc, const char * argv[]) {
     
-    int limitNumber;
-    int divideFlg = 1;
-    int quotientNum;
-    int remainder;
+    char input[20];
+    int inputSize;
+    int i;
+    int decimalNum;
     
-    printf("What Decimal number is to get get Binary numer : ");
-    scanf("%i",&limitNumber);
+    printf("Please Enter Binary Number: ");
+    gets(input);
     
-    int i = 0;
-    int tempNum = limitNumber;
-    while (divideFlg) {
-        quotientNum = tempNum/2;
-        if (quotientNum == 1) {
-            divideFlg = 0;
-        }else{
-            tempNum = quotientNum;
-        }
-        i++;
-    }
+    inputSize = (int)strlen(input);
     
-    int binaryNum[i+1];
-    
-    
-    divideFlg = 1;
-    i =0;
-    while (divideFlg) {
+    decimalNum = 0;
+    for (i = 0; i < inputSize; i++) {
+        decimalNum += (input[i]-'0') * pow((float)2, inputSize-i-1);
+        printf("fk %i\n",decimalNum);
         
-        quotientNum = limitNumber/2;
-        remainder = limitNumber%2;
-        
-        if (quotientNum == 1) {
-            divideFlg = 0;
-            binaryNum[i] = remainder;
-            binaryNum[i+1] = quotientNum;
-
-        }else{
-            limitNumber = quotientNum;
-            binaryNum[i] = remainder;
-
-        }
-        i++;
     }
-    printf("The binay number is ");
-    for (int j= 0; j < i+1; j++) {
-        printf("%i",binaryNum[i-j]);
-
-    }
-    printf("\n");
+    
+    printf("Decimal Number is %i\n",decimalNum);
+    
+    
+    
+//    int limitNumber;
+//    int divideFlg = 1;
+//    int quotientNum;
+//    int remainder;
+//    
+//    printf("What Decimal number is to get get Binary numer : ");
+//    scanf("%i",&limitNumber);
+//    
+//    int i = 0;
+//    int tempNum = limitNumber;
+//    while (divideFlg) {
+//        quotientNum = tempNum/2;
+//        if (quotientNum == 1) {
+//            divideFlg = 0;
+//        }else{
+//            tempNum = quotientNum;
+//        }
+//        i++;
+//    }
+//    
+//    int binaryNum[i+1];
+//    
+//    
+//    divideFlg = 1;
+//    i =0;
+//    while (divideFlg) {
+//        
+//        quotientNum = limitNumber/2;
+//        remainder = limitNumber%2;
+//        
+//        if (quotientNum == 1) {
+//            divideFlg = 0;
+//            binaryNum[i] = remainder;
+//            binaryNum[i+1] = quotientNum;
+//
+//        }else{
+//            limitNumber = quotientNum;
+//            binaryNum[i] = remainder;
+//
+//        }
+//        i++;
+//    }
+//    printf("The binay number is ");
+//    for (int j= 0; j < i+1; j++) {
+//        printf("%i",binaryNum[i-j]);
+//
+//    }
+//    printf("\n");
 
     
     
